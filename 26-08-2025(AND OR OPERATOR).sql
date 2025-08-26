18f28fec1a78ba3de26b560ca2f6e46c1f0159c1
sagar12900 <hemasagar6421@gmail.com>
2025-08-26 14:32:49 +0530

    Add files via upload

---
+++ 26-08-2025(AND OR OPERATOR).sql
@@ -0,0 +1,12 @@
+create DATABASE book;
+USE book;
+CREATE TABLE colleges (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(100),city VARCHAR(50),state VARCHAR(50),ranking INT,students_count INT);
+INSERT INTO colleges (name, city, state, ranking, students_count) VALUES
+('sathyabama University', 'chennai', 'tamilnadu', 50, 12000),
+('gitam university', 'vizag', 'andhrapradesh', 30, 8000),
+('srm', 'chennai', 'tamilnadu', 25, 15000),
+('amritha university', 'banglore', 'karnataka', 40, 9000),
+('veltech', 'chennai', 'tamilnadu', 35, 11000);
+SELECT * FROM colleges
+WHERE (state = 'tamilnadu' AND ranking < 45)
+   OR (state = 'andhrapradesh' AND students_count > 10000);    
